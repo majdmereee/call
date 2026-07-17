f6d510ce88bfe9574e699f4228ee2d6c76d0be44
majdmereee <majdmereee@gmail.com>
2026-07-17 20:17:14 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  // رح نعدل هاد الكود بعد ما تجيبي ملف الـ json من فايربيس
-  // await Firebase.initializeApp(); 
+  // التهيئة اليدوية المباشرة باستخدام بيانات مشروعك الفعلي من الملف
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBochg6o4aY5nYK_hgSA-OKPPJBWNq4W8g",
+      appId: "1:561315856725:android:fad6b8868605563cacafdc",
+      messagingSenderId: "561315856725", // هو نفسه الـ project_number
+      projectId: "soos-e2bbb",
+    ),
+  );
 
   runApp(const MyApp());
 }
@@ -23,7 +30,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: Center(
-          child: Text('جاري إعداد التطبيق...'),
+          child: Text('Firebase متصل بنجاح بمشروع soos!'),
         ),
       ),
     );
